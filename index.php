9681401d6ed3a07edd355625f3df35a36fc55630
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-11-22 23:51:12 +0100

    Remove passwords from config file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 if (!file_exists($path)) {
