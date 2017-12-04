bbe9e61c54d670fb4770e0911a5f8f8df94eabfc
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-12-04 23:38:04 +0100

    Add UglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const dev = process.env.NODE_ENV === 'development'
 
 let config = {
   entry: path.resolve('./src/Frontend/assets/js/index.js'),
   output: {
     path: path.resolve('./public/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: "/dist/"
   },
-  watch: true,
+  watch: dev,
+  devtool: dev ? 'cheap-module-eval-source-map' : 'source-map',
   module: {
     rules: [
       {
@@ -15,7 +20,14 @@ let config = {
         use: ['babel-loader']
       }
     ]
-  }
+  },
+  plugins: []
+}
+
+if (!dev) {
+  config.plugins.push(new UglifyJsPlugin({
+    sourceMap: true
+  }))
 }
 
 module.exports = config
