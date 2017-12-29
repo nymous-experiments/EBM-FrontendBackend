a5c5366774603eca32282af84eefa8e2ef0e12c7
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-12-29 19:32:03 +0100

    Refactor: extract onkeydown handlers

--- src/Frontend/components/article/article.js
+++ src/Frontend/components/article/article.js
@@ -1,10 +1,12 @@
 import $ from 'jquery'
 
-import {getArticle, setArticleTitle} from '@services/article.service'
-import {setParagraphContent} from '@services/paragraphs.service'
+import {getArticle} from '@services/article.service'
 
 import {SET_ARTICLE} from './article.customEvents'
-import {hideArticle, hideNoArticleSelectedMessage, setArticle, showSpinner} from './article.utils'
+import {
+  handleParagraphKeydown, handleTitleKeydown, hideArticle, hideNoArticleSelectedMessage, setArticle,
+  showSpinner
+} from './article.utils'
 import {articleParagraphsContainer, articleTitle} from './article.selectors'
 
 $(document).on(SET_ARTICLE, function (event, articleId) {
@@ -18,22 +20,14 @@ $(document).on(SET_ARTICLE, function (event, articleId) {
 
 articleTitle.click(function (event) {
   const target = $(event.target)
+  // Avoid firing the event once the title has been replace with the input
   if (target.is('h1')) {
     const metadata = target.data('metadata')
     const textInput = $(`<input class="input title" type="text">`).val(metadata.title)
+
     textInput.data('previousMetadata', metadata)
-    textInput.keydown(function (event) {
-      if (event.which === 13) { // Enter
-        const thisTextinput = $(event.target)
-        const newTitle = thisTextinput.val()
-        const newMetadata = Object.assign({}, thisTextinput.data('previousMetadata'), {title: newTitle})
-        const titleToReplace = $(`<h1 class="title">${newTitle}</h1>`)
-        titleToReplace.data('metadata', newMetadata)
-        setArticleTitle(newMetadata.id, newTitle)
-          .then(() => thisTextinput.replaceWith(titleToReplace))
-          .catch(err => console.error(err)) // TODO Handle error
-      }
-    })
+    textInput.keydown(handleTitleKeydown)
+
     target.html(textInput)
     textInput.focus()
   }
@@ -45,24 +39,12 @@ articleParagraphsContainer.click(function (event) {
   if (target.is('p')) {
     const metadata = target.data('metadata')
     const textarea = $(`<textarea class="textarea article-paragraph"></textarea>`).val(metadata.content)
+
     textarea.data('previousMetadata', metadata)
-
-    textarea.keydown(function (event) {
-      if (event.which === 13) { // Enter
-        const thisTextarea = $(event.target)
-        const newContent = thisTextarea.val()
-        const newMetadata = Object.assign({}, thisTextarea.data('previousMetadata'), {content: newContent})
-        const paragraphToReplace = $(`<p class="article-paragraph" data-order="${newMetadata.order}">${newContent}</p>`)
-        paragraphToReplace.data('metadata', newMetadata)
-        setParagraphContent(newMetadata.id, newContent)
-          .then(() => thisTextarea.replaceWith(paragraphToReplace))
-          .catch(err => console.error(err)) // TODO Handle error
-      }
-    })
-
+    textarea.keydown(handleParagraphKeydown)
     const toInsert = $(`<div></div>`).html(textarea)
-    target.replaceWith(toInsert)
 
+    target.replaceWith(toInsert)
     textarea.focus()
   }
 })
