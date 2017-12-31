9cab44e3a5ad711c26fa5e5b4dd33d13579bddb0
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-12-31 12:56:40 +0100

    Fix OPTIONS requests

--- src/api.php
+++ src/api.php
@@ -135,7 +135,14 @@ switch ($real_route[0]) {
         break;
 }
 
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    $response = "";
+}
+
 header("Content-Type: application/json");
+
 header("Access-Control-Allow-Origin: https://editor.swagger.io");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 
